9165c8e0aef16e90d4877f2916023bc3d90f58eb
AddisonLO <addisonlohohim@gmail.com>
2026-08-18 23:26:33 +0800

    numerous updates to the website

--- scripts/index.js
+++ scripts/index.js
@@ -10,3 +10,30 @@ function toggleVisible() {
         inputBar.style.display = "none";
     }
 }
+
+const launches = document.querySelector(".launches");
+
+fetch("../data/new_information.json")
+    .then(res => res.json())
+    .then(data => {
+        const updates = document.querySelector(".updates");
+        
+
+        data.forEach(item => {
+            const articleRow = document.createElement("div");
+            articleRow.classList.add("row");
+
+            articleRow.innerHTML = `
+                <h3>${item.title}</h3>
+                <p><strong>Date:</strong> ${item.date}</p>
+                <p><strong>Description:</strong>  ${item.description}</p>
+                <hr>
+            `;
+
+            if (item.type === "update") {
+                updates.appendChild(articleRow);
+            } else {
+                launches.appendChild(articleRow);
+            }
+        });
+    });
